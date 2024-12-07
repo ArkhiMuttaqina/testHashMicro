08668c652f855c559a85f19505a483b6380826fe
crackgenius <arkhi07@hotmail.co.id>
2024-12-07 11:44:01 +0700

    initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CitiesTableSeeder::class);
-        $this->call(JobTitlesTableSeeder::class);
         $this->call(DepartmentTableSeeder::class);
+        $this->call(JobTitlesTableSeeder::class);
         $this->call(UsersTableSeeder::class); // EXAMPLE PURPOSE ONLY FOR TEST RECRUITMENT
 
     }
